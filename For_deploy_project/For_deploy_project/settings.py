0e107759652f9fe13b0eafb6a93730fababf97cb
Berkut <brekutsgaming@mail.ru>
2021-12-28 23:17:12 +0500

    Made vars for static files in 'settings.py'
    run 'python manage.py collectstatic' command in console

--- For_deploy_project/For_deploy_project/settings.py
+++ For_deploy_project/For_deploy_project/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+if DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'
+else:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+
 MEDIA_URL = 'media/'
 
 # Default primary key field type
